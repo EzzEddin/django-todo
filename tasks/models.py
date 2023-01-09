b52af4cb6e18c78738bafc3d79a922f037e44d96
ezzeddin <ezzeddinabdullah@gmail.com>
2023-01-09 14:04:49 +0200

    Change title attribute to text

--- tasks/models.py
+++ tasks/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=200)
     is_done = models.BooleanField(default=False)
     created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return self.text
